570c461a4654d8d714c12f5c9e46f3a62086ac49
Nikita Jayaprakash <nikita.jp@gmail.com>
2023-06-04 20:09:26 -0700

    process-sid functions

---
+++ R/helperscripts/process-sid.R
@@ -0,0 +1,77 @@
+process_sids <- function(new_data) {
+    df_filtered <- new_data %>% filter(!is.na(new_data$sid))
+    
+    duplicated_rows <- df_filtered %>%
+        group_by(sid) %>%
+        filter(n() > 1) %>%
+        ungroup()
+    
+    duplicates_df <- new_data %>%
+        filter(is.na(sid) | (sid %in% duplicated_rows$sid)) %>%
+        arrange(sid)
+    
+    #df with only unique sid
+    unique_sids <- new_data[!(new_data$sid %in% duplicates_df$sid), ]
+    
+    #combined df with unique and na in sid
+    unique_and_na_sids <- rbind(unique_sids, filter(duplicates_df, is.na(sid)))
+    
+    #df with duplicate sid so we can do merging using only this small df
+    duplicate_sids_without_na <- filter(duplicates_df, !is.na(sid))
+    
+    if (nrow(duplicate_sids_without_na) > 0) {
+        data_uniquesids <- duplicate_sids_without_na %>%
+            group_by(sid) %>%
+            summarize(across(
+                everything(),
+                ~ if (inherits(., "Period")) {
+                    last(na.omit(.))
+                } else if (is.numeric(.)) {
+                    max(., na.rm = TRUE)
+                } else {
+                    if (all(is.na(.))) {
+                        NA
+                    } else {
+                        last(na.omit(.))
+                    }
+                }
+            )) %>%
+            ungroup()
+        
+        #combine dataframes into 1 correct dataframe of students
+        result <- rbind(unique_and_na_sids, data_uniquesids)
+    } else {
+        result <- new_data
+    }
+    
+    return(list(unique_sids = result, duplicates = duplicates_df))
+}
+
+
+pivot <- function(new_data, assignments_dataframe, cat_table){
+    id_cols <- c("names", "sections","email", "sid")
+    
+    new_data <- as.data.frame(new_data)
+    
+    sxa <- new_data %>%
+        pivot_longer(!all_of(id_cols), # change the unit of obs to student x assignment
+                     names_to = c("assignments", ".value"),
+                     names_sep = "_-_") %>%
+        replace_na(list(raw_points = 0))
+    
+    if (!is.null(assignments_dataframe)) {
+        assignments_dataframe$new_colnames <- str_replace_all(assignments_dataframe$new_colnames, "_-_raw_points", "")
+    }
+    
+    add_categories_to_pivot <- sxa %>%
+        left_join(assignments_dataframe %>% select(new_colnames, colnames, category), by = c("assignments" = "new_colnames"))
+    if (!is.null(cat_table)){
+        add_cat_table_to_pivot <- add_categories_to_pivot %>%
+            left_join(cat_table %>% select(Categories, Weights, Drops, Grading_Policy, Clobber_Policy, Late_Policy), by = c("category"="Categories"))
+        return(add_cat_table_to_pivot)
+    }
+    else{
+        return(add_categories_to_pivot)
+    }
+    
+}
