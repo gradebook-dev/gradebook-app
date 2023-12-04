ae2b7ee8252d141dc44cee8db1ace32487b8d761
Monika Voutov <50851977+mvoutov@users.noreply.github.com>
2023-12-04 12:50:25 -0800

    Revert "Integrate package"

--- R/helperscripts/assignments.R
+++ R/helperscripts/assignments.R
@@ -1,3 +1,32 @@
+#create assignment table from input data
+createAssignTable <- function(data) {
+    gs_cols <- names(data)
+    
+    #TEMPORARY SOLUTION FOR DIFFERING COLNAMES
+    
+    #data 88E specific
+    if (gs_cols[1] == "Name"){
+        gs_cols[1] <- "Names"
+    }
+    
+    #stat 20 specific - TBD
+    
+    assignments <- tibble(colnames = gs_cols) %>%
+        #General regex to rename assignments and add a column "category" in table
+        mutate(new_colnames = str_replace_all(tolower(colnames), "[\\s:]+", "_"),
+               #     category = substr(str_replace_all(tolower(colnames), "[\\s:]+", "_"), 1, regexpr("_", str_replace_all(tolower(colnames), "[\\s:-]+", "_")) - 1),
+               type = if_else(!str_detect(new_colnames, "name|sections|max|time|late|email|sid"), "_-_raw_points", "" ),
+               new_colnames = paste0(new_colnames, type))%>% # concatenate gs_col and type
+        select(new_colnames, colnames) %>%
+        mutate(category = "Unassigned")
+    
+    # Replace all "," with ":" in the colnames to avoid the issue with selecting these assignments in creating categories.
+    assignments$colnames <- str_replace_all(assignments$colnames, ",", ":")
+    assignments$new_colnames <- str_replace_all(assignments$new_colnames, ",", ":")
+    
+    return(assignments)
+}
+
 getUnassigned <- function(assign_table){
     left <-assign_table %>% 
         filter(category == "Unassigned")
