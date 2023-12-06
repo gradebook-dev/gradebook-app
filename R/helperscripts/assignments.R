0aabbc6dc3435e91be81b9010cf1f7f95b1d5b93
Nikita Jayaprakash <nikita.jp@gmail.com>
2023-12-05 17:42:26 -0800

    emptying out app

--- R/helperscripts/assignments.R
+++ R/helperscripts/assignments.R
@@ -1,30 +1,13 @@
 getUnassigned <- function(assign_table){
-    left <-assign_table %>% 
-        filter(category == "Unassigned")
-    if (nrow(left) != 0){
-        return (left$colnames) 
-    }
-    return ("No more new assignments")
+    
 }
 
 #updates assignments in assignment table when they are assigned a category
 updateAssigns <- function(assigns_table, assignments, original_cat, new_cat){
-    assigns_table <- resetAssigns(assigns_table, original_cat)
-    if (!is.null(assignments)){
-        selected <- data.frame(colnames = assignments)
-        selected <- semi_join(assigns_table, selected, "colnames") %>% mutate(category = new_cat)
-        assigns_table <- rbind(selected, anti_join(assigns_table, selected, "colnames"))
-    }
-    return (assigns_table)
+   
 }
 
 #resets all assignments of a category to "Unassigned"
 resetAssigns <- function(assigns_table, original_category){
-    if (!is.null(original_category)){
-    selected <- assigns_table %>%
-        filter(category == original_category) %>%
-        mutate(category = "Unassigned")
-    assigns_table <- rbind(selected, anti_join(assigns_table, selected, "colnames"))
-    }
-    return (assigns_table)
+    
 }
