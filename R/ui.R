ee6159448148177801f067783f776bc050feb983
mvoutov <mvoutov@gmail.com>
2024-02-21 10:22:01 -0800

    fix appearance

--- R/ui.R
+++ R/ui.R
@@ -37,18 +37,69 @@ shinyUI(
                 div(style = "display: flex; align-items: center; margin-left: 15px;",
                     h5("Download Your Course Grades:")
                 ),
-                downloadButton("download_grades","Download Grades"),
+                div(style = "display: flex; align-items: center; margin-left: 15px;",
+                downloadButton("download_grades","Download Grades")
+                ),
                 hr(),
                 div(style = "display: flex; align-items: center; margin-left: 15px;",
                     h5("Download Your Yaml Policy File:")
                 ),
-                downloadButton("download_policy_file","Download Policy File")
-                # div(style = "display: flex; align-items: center; margin-left: 15px;",
-                #     downloadButton("download_policy_file","Download Policy File")
-                # )
+                div(style = "display: flex; align-items: center; margin-left: 15px;",
+                    downloadButton("download_policy_file","Download Policy File")
+                )
             )
         ),
         dashboardBody(
+            tags$head(tags$style(HTML('
+                                /* logo */
+                                .skin-blue .main-header .logo {
+                                background-color:#50A5EA;
+                                color: #1B2A3A;
+                               # font-family: "Georgia", Times, "Times New Roman";
+                               # font-weight: bold;
+                                font-size: 24px;
+                                }
+                                
+                                /* logo when hovered */
+                                .skin-blue .main-header .logo:hover {
+                                background-color: #50A5EA;
+                               
+                                }
+                                
+                                /* navbar (rest of the header) */
+                                .skin-blue .main-header .navbar {
+                                background-color: #50A5EA;
+                                }
+                                
+                                /* main sidebar */
+                                .skin-blue .main-sidebar {
+                                background-color: #2E3537;
+                                }
+                                
+                                /* active selected tab in the sidebarmenu */
+                                .skin-blue .main-sidebar .sidebar .sidebar-menu .active a{
+                                background-color: #316590;
+                                color: #FFFFFF;
+                                }
+                                
+                                /* other links in the sidebarmenu */
+                                .skin-blue .main-sidebar .sidebar .sidebar-menu a{
+                                background-color: #2E3537;
+                                color: #FFFFFF;
+                                }
+                                
+                                /* other links in the sidebarmenu when hovered */
+                                .skin-blue .main-sidebar .sidebar .sidebar-menu a:hover{
+                                background-color: #438BC5;
+                                }
+                               s
+                                /* body */
+                                .content-wrapper, .right-side {
+                                background-color: #ffffff; 
+                            
+                                }
+                                 
+                                '))),
             tabItems(
                 Files,
                 Policies
