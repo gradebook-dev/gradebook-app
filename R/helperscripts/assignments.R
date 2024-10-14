6fa52563276457e48261a14bcf87b319d5a66a5d
mvoutov <mvoutov@gmail.com>
2024-10-13 17:45:53 -0700

    allow duplicate assignments.

--- R/helperscripts/assignments.R
+++ R/helperscripts/assignments.R
@@ -1,8 +1,9 @@
 
 #returns list of assignments with no assigned category
 getUnassigned <- function(assign_table){
-    assigns <- assign_table[["assignment"]][assign_table$category == "Unassigned"]
+    assigns <- assign_table[["assignment"]] #[assign_table$category == "Unassigned"]
     if (length(assigns) != 0){
+        print(assigns)
         return (assigns) 
     }
     return ()
